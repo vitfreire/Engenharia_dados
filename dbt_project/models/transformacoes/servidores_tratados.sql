20c48c1d83156fc42cca6bd5d5eef014ab450fb0
vitoria freire <vitoria.loruama.freire@gmail.com>
2025-07-10 22:29:36 -0300

    feat: adiciona testes e tratamento de duplicatas no modelo servidores_tratados

--- dbt_project/models/transformacoes/servidores_tratados.sql
+++ dbt_project/models/transformacoes/servidores_tratados.sql
@@ -1,12 +1,34 @@
 WITH base AS (
     SELECT
-        nome,
-        REPLACE(REPLACE(total, '.', ''), ',', '.')::numeric AS total_convertido
-    FROM {{ ref('servidores') }}
+        *,
+      ROW_NUMBER() OVER (PARTITION BY cpf ORDER BY total_convertido DESC) AS linha
+    FROM (
+        SELECT
+            nome,
+            cpf,
+            funcionario_id,
+            descricao_orgao,
+            codigo_orgao,
+            numero_ordem,
+            id,
+            CAST(SPLIT_PART(REPLACE(REPLACE(total, '.', ''), ',', '.'), 'R$', 1) AS NUMERIC) AS total_convertido,
+            CAST(ano AS INT) AS ano,
+            CAST(mes AS INT) AS mes
+        FROM {{ ref('servidores') }}
+    ) sub
 )
 
 SELECT
     nome,
-    total_convertido,
-    total_convertido * 0.1 AS horas_extras
+    cpf,
+    funcionario_id,
+    descricao_orgao,
+    codigo_orgao,
+    numero_ordem,
+    id,
+    total_convertido AS total,
+    total_convertido * 0.1 AS horas_extras,
+    ano,
+    mes
 FROM base
+WHERE linha = 1
